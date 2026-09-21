5bfea968e32bbe44af4f0cbf4a9dc5df04134286
KimDmitriyR <kdmitrikim@yandex.ru>
2026-09-21 11:18:16 +0300

    feat: add fast api backend

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title = "NeuroQuest API",
+    version = "0.1.0",
+)
+
+@app.get("/api/health")
+async def health_check():
+    return{
+        "status" : "ok"
+    }
